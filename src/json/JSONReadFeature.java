6d965bfe6e8d40507cd65893aaba8ab166270e91
Oleg Sobolev <sobolev_ov@mail.ru>
2025-01-16 22:14:19 +0100

    Comments

--- src/json/JSONReadFeature.java
+++ src/json/JSONReadFeature.java
@@ -7,17 +7,32 @@ public enum JSONReadFeature {
      * "&bsol;u41" -> "A" (allow unicode number be less than 4 hex digits)
      */
     INVALID_ESCAPES,
+    /**
+     * {@code 'xyzzy' -> "xyzzy"}
+     */
     SINGLE_QUOTES,
     STRING_CONTROL_CHARS,
+    /**
+     * {@code { x: "value" } -> { "x": "value: }}
+     */
     UNQUOTED_FIELD_NAMES,
+    /**
+     * {@code [1,,,2,] -> [1,null,null,2,null]}
+     */
     ARRAY_MISSING_VALUES,
+    /**
+     * {@code { "x": "value", } -> { "x": "value" }}
+     * {@code [1,2,] -> [1,2]}
+     * <br>
+     * Takes priority over {@link #ARRAY_MISSING_VALUES} if both are present.
+     */
     TRAILING_COMMA,
     /**
      * Recognize following numeric values (case-insensitive):
-     * NaN,
+     * {@code NaN,
      * Infinity, inf,
      * +Infinity, +inf,
-     * -Infinity, -inf,
+     * -Infinity, -inf}
      */
     NAN_INF_NUMBERS,
     /**
