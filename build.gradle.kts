8edc11e71fa2db72346d14192155f16db88804c3
Oleg Sobolev <sobolev_ov@mail.ru>
2025-01-17 05:24:48 +0100

    Prepare for unit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,12 @@ plugins {
 
 group = "io.github.osobolev"
 version = "1.0"
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
