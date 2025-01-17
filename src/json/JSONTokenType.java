eb667fc99f414dc24083a8f13fb060fedcbe1172
Oleg Sobolev <sobolev_ov@mail.ru>
2025-01-17 01:32:43 +0100

    Pretty print for token type

--- src/json/JSONTokenType.java
+++ src/json/JSONTokenType.java
@@ -1,7 +1,23 @@
 package json;
 
 public enum JSONTokenType {
-    LCURLY, RCURLY, LSQUARE, RSQUARE, COMMA, COLON,
-    STRING, IDENT, FLOAT, INT, IDENT_FLOAT, TRUE, FALSE, NULL,
-    EOF
+    LCURLY("{"), RCURLY("}"), LSQUARE("["), RSQUARE("]"), COMMA(","), COLON(":"),
+    TRUE("true"), FALSE("false"), NULL("null"),
+    STRING, IDENT, FLOAT, INT, IDENT_FLOAT,
+    EOF;
+
+    private final String text;
+
+    JSONTokenType(String symbol) {
+        this.text = symbol == null ? name() : "'" + symbol + "'";
+    }
+
+    JSONTokenType() {
+        this(null);
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
 }
