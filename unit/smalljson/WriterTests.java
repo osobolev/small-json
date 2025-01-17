464e46476fc29745589a9407d5b56cbd4c05f5fc
Oleg Sobolev <sobolev_ov@mail.ru>
2025-01-17 10:21:02 +0100

    Test for JSONWriter

--- unit/smalljson/WriterTests.java
+++ unit/smalljson/WriterTests.java
@@ -1,6 +1,7 @@
 package smalljson;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 
@@ -9,19 +10,20 @@ import static smalljson.TestUtil.parse;
 
 public class WriterTests {
 
-    @Test
-    public void testWriter() throws IOException {
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  "})
+    public void testWriter(String indent) throws IOException {
         JSONParseOptions options = SiteUtil.siteOptions();
         SiteUtil.scanSiteTests((name, failing, is) -> {
             if (failing)
                 return;
             Object origObj = new JSONParser(options, is).parse();
-            String json1 = JSONWriter.toString(origObj);
+            String json1 = JSONWriter.toString(origObj, indent);
 
             Object newObj = parse(json1);
             assertEquals(origObj, newObj);
 
-            String json2 = JSONWriter.toString(newObj);
+            String json2 = JSONWriter.toString(newObj, indent);
             assertEquals(json1, json2);
         });
     }
