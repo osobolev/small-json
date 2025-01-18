2f32c719ca30753adfa3dca6f99eac270e891a3c
Oleg Sobolev <sobolev_ov@mail.ru>
2025-01-18 02:25:49 +0100

    Access to options

--- src/smalljson/JSON.java
+++ src/smalljson/JSON.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 
 public final class JSON {
 
-    private final JSONParseOptions options;
+    public final JSONParseOptions options;
 
     public JSON(JSONParseOptions options) {
         this.options = options;
