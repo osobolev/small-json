3fc5e47486b4a8390801f22d0340d7ae57c4d828
Oleg Sobolev <sobolev_ov@mail.ru>
2025-01-18 06:17:17 +0100

    Project description for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+description = "Small JSON library"
+
 plugins {
     `module-lib`
 }
@@ -12,4 +14,27 @@ dependencies {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+(publishing.publications["mavenJava"] as MavenPublication).pom {
+    name.set("small-json")
+    description.set("Small JSON library")
+    url.set("https://github.com/osobolev/small-json")
+    licenses {
+        license {
+            name.set("The Apache License, Version 2.0")
+            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+        }
+    }
+    developers {
+        developer {
+            name.set("Oleg Sobolev")
+            organizationUrl.set("https://github.com/osobolev")
+        }
+    }
+    scm {
+        connection.set("scm:git:https://github.com/osobolev/small-json.git")
+        developerConnection.set("scm:git:https://github.com/osobolev/small-json.git")
+        url.set("https://github.com/osobolev/small-json")
+    }
 }
