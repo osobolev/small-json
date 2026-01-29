97910210a17f97a86566190747c981b7c1621a54
Oleg Sobolev <sobolev_ov@mail.ru>
2026-01-29 19:34:32 +0100

    Plugin update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import com.vanniktech.maven.publish.JavadocJar
 description = "Small JSON library"
 
 plugins {
-    id("com.vanniktech.maven.publish") version "0.35.0"
+    id("com.vanniktech.maven.publish") version "0.36.0"
     `module-lib`
 }
 
