b36bc2b9d9074db102d304ae2b35424175b93a65
Oleg Sobolev <sobolev_ov@mail.ru>
2026-05-03 11:39:40 +0200

    Removed unused constant

--- src/smalljson/JSONWriter.java
+++ src/smalljson/JSONWriter.java
@@ -9,8 +9,6 @@ import java.util.Map;
 
 public final class JSONWriter {
 
-    public static final String DEFAULT_INDENT = "    ";
-
     public interface RawValue {
 
         String rawJsonOutput();
