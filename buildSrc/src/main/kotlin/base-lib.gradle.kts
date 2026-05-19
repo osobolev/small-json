5f4ef13f2ad70f2d2b9c3fba0061d368ac633b88
Oleg Sobolev <sobolev_ov@mail.ru>
2026-05-20 00:19:31 +0200

    Dependencies update

--- buildSrc/src/main/kotlin/base-lib.gradle.kts
+++ buildSrc/src/main/kotlin/base-lib.gradle.kts
@@ -35,7 +35,7 @@ configurations["manualCompileOnly"].extendsFrom(configurations["compileOnly"])
 dependencies {
     "manualImplementation"(sourceSets["main"].output)
 
-    testImplementation("org.junit.jupiter:junit-jupiter:6.0.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:6.1.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
